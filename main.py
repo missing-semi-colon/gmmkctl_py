1c702cf73872aa986501bb01f229dd90e825d13e
missing-semi-colon <67431858+missing-semi-colon@users.noreply.github.com>
2021-06-07 19:44:54 +0100

    Add license notices

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
+"""
+Copyright 2021 paulguy <paulguy119@gmail.com>
+Copyright 2021 missing-semi-colon <https://github.com/missing-semi-colon>
+
+This file is part of gmmkctl_py.
+
+gmmkctl_py is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+gmmkctl_py is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with gmmkctl_py.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 import sys
 from typing import Generator, Callable
 
