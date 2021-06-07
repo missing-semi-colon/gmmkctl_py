361cf4afee5ae84c30b788a871ab531aa94af8fa
missing-semi-colon <67431858+missing-semi-colon@users.noreply.github.com>
2021-06-07 10:18:24 +0100

    Initial commit

---
+++ gmmk.py
@@ -0,0 +1,187 @@
+import usb.core
+import usb.util
+from typing import Union
+
+VENDOR_ID = 0x0C45
+PRODUCT_ID = 0x652F
+ENDPOINT_OUT = 0x03
+ENDPOINT_IN = 0x82
+PACKET_SIZE = 64
+TIMEOUT_MS = 1000
+
+MAX_KEY = 126
+
+SUM_OFFSET = 1
+COMMAND_OFFSET = 3
+
+CMD_START = 1
+CMD_END = 2
+
+CMD_KEYCOLORS = 0x11
+KEYCOLORS_COUNT_OFFSET = 4
+KEYCOLORS_START_OFFSET = 5
+KEYCOLORS_DATA_OFFSET = 8
+KEYCOLORS_DATA_SIZE = PACKET_SIZE - KEYCOLORS_DATA_OFFSET
+
+CMD_SUBCOMMAND = 0x06
+SUBCMD_CMD_OFFSET = 4
+SUBCMD_ARG_OFFSET = 8
+
+SUBCMD_MODE = [0x01, 0x00, 0x00, 0x00]
+SUBCMD_BRIGHTNESS = [0x01, 0x01, 0x00, 0x00]
+SUBCMD_DELAY = [0x01, 0x02, 0x00, 0x00]
+SUBCMD_DIRECTION = [0x01, 0x03, 0x00, 0x00]
+SUBCMD_COLORFUL = [0x01, 0x04, 0x00, 0x00]
+SUBCMD_COLOR = [0x03, 0x05, 0x00, 0x00]
+SUBCMD_RATE = [0x01, 0x0f, 0x00, 0x00]
+
+ARG_DIRECTION_LEFT = 0xFF
+ARG_DIRECTION_RIGHT = 0
+ARG_COLORFUL = 1
+ARG_NOT_COLORFUL = 0
+
+
+def get_gmmk() -> usb.core.Device:
+	""" Returns the first GMMK found """
+	device = usb.core.find(idVendor=VENDOR_ID, idProduct=PRODUCT_ID)
+	if device is None:
+		raise ValueError("Device not found")
+		return None
+	return device
+
+def setup(device: usb.core.Device) -> None:
+	""" Obtains control of `device` """
+	# Release control of the keyboard from anything that has it
+	_detach_kernel_drivers(device)
+	# Set configuration to the first found
+	device.set_configuration()
+
+def send_cmd(device: usb.core.Device, cmd: list, cmd_arg: Union[int, list]) -> None:
+	_cmd_start(device)
+	_cmd_body(device, cmd, cmd_arg)
+	_cmd_end(device)
+
+def set_mode(device: usb.core.Device, mode: int) -> None:
+	send_cmd(device, SUBCMD_MODE, mode)
+
+def set_brightness(device: usb.core.Device, brightness: int) -> None:
+	send_cmd(device, SUBCMD_BRIGHTNESS, brightness)
+
+def set_delay(device: usb.core.Device, delay: int) -> None:
+	send_cmd(device, SUBCMD_DELAY, delay)
+
+def set_direction_left(device: usb.core.Device) -> None:
+	send_cmd(device, SUBCMD_DIRECTION, ARG_DIRECTION_LEFT)
+
+def set_direction_right(device: usb.core.Device) -> None:
+	send_cmd(device, SUBCMD_DIRECTION, ARG_DIRECTION_RIGHT)
+
+def set_colorful(device: usb.core.Device) -> None:
+	send_cmd(device, SUBCMD_COLORFUL, ARG_COLORFUL)
+
+def set_not_colorful(device: usb.core.Device) -> None:
+	send_cmd(device, SUBCMD_COLORFUL, ARG_NOT_COLORFUL)
+
+def set_color(device: usb.core.Device, r: int, g: int, b: int) -> None:
+	send_cmd(device, SUBCMD_COLOR, [r, g, b])
+
+def set_rate(device, rate) -> None:
+	""" Set the polling rate """
+	send_cmd(device, SUBCMD_RATE, rate)
+
+def set_keys(device: usb.core.Device, start: int, count: int, colors: list) -> None:
+	"""
+	Sets the colors of the keys, starting with the key with number `start` and
+	the following `count` keys
+
+	Args:
+		device: a GMMK
+		start: number of the starting key
+		count: number of keys
+		colors: colors to set the keys
+	"""
+	if start > MAX_KEY:
+		return
+	if start + count > MAX_KEY:
+		count = MAX_KEY - start
+
+	_cmd_start(device)
+
+	start_key_offset = 0
+	while start_key_offset < count:
+		buffer = [0]*PACKET_SIZE
+		buffer[0] = 4
+
+		buffer[COMMAND_OFFSET] = CMD_KEYCOLORS
+
+		# Number of key's colors to send in the next packet
+		packet_key_count = min(count - start_key_offset, KEYCOLORS_DATA_SIZE // 3)
+		# Number of the starting key to send in the next packet
+		packet_key_start = start + start_key_offset
+		# Each color has 3 bytes
+		buffer[KEYCOLORS_COUNT_OFFSET] = packet_key_count * 3
+		# The start offset must be split over 2 bytes
+		buffer[KEYCOLORS_START_OFFSET:KEYCOLORS_START_OFFSET+2] = [
+			(packet_key_start * 3) % 256,
+			(packet_key_start * 3) // 256 ]
+
+		# Write the color bytes into the buffer
+		color_byte_offset = 0
+		while color_byte_offset < buffer[KEYCOLORS_COUNT_OFFSET]:
+			color_idx = start_key_offset + (color_byte_offset // 3)
+			buffer[KEYCOLORS_DATA_OFFSET+color_byte_offset]   = colors[color_idx][0]
+			buffer[KEYCOLORS_DATA_OFFSET+color_byte_offset+1] = colors[color_idx][1]
+			buffer[KEYCOLORS_DATA_OFFSET+color_byte_offset+2] = colors[color_idx][2]
+			color_byte_offset += 3
+
+		buffer[SUM_OFFSET:COMMAND_OFFSET] = _get_buffer_sum(buffer)
+
+		packet = bytes(buffer)
+		device.write(ENDPOINT_OUT, packet)
+		device.read(ENDPOINT_IN, PACKET_SIZE, TIMEOUT_MS)
+		start_key_offset += packet_key_count
+
+	_cmd_end(device)
+
+def _detach_kernel_drivers(device: usb.core.Device) -> None:
+	""" Detaches active drivers so that the device can be controlled """
+	c = 1
+	for cfg in device:
+		for i in range(cfg.bNumInterfaces):
+			if device.is_kernel_driver_active(i):
+				try:
+					device.detach_kernel_driver(i)
+				except usb.core.USBError as e:
+					print("Could not detach kernel driver")
+		c += 1
+
+def _cmd_start(device: usb.core.Device) -> None:
+	buffer = [0]*PACKET_SIZE
+	buffer[COMMAND_OFFSET] = CMD_START
+	packet = bytes(buffer)
+	device.write(ENDPOINT_OUT, packet)
+
+def _cmd_body(device: usb.core.Device, cmd: list, cmd_arg: Union[int, list]) -> None:
+	buffer = [0]*PACKET_SIZE
+	buffer[COMMAND_OFFSET] = CMD_SUBCOMMAND
+	buffer[SUBCMD_CMD_OFFSET:SUBCMD_CMD_OFFSET + len(cmd)] = cmd
+	if not (type(cmd_arg) is list):
+		buffer[SUBCMD_ARG_OFFSET] = cmd_arg
+	else:
+		buffer[SUBCMD_ARG_OFFSET:SUBCMD_ARG_OFFSET+len(cmd_arg)] = cmd_arg
+	packet = bytes(buffer)
+	device.write(ENDPOINT_OUT, packet)
+	device.read(ENDPOINT_IN, PACKET_SIZE, TIMEOUT_MS)
+
+def _cmd_end(device: usb.core.Device) -> None:
+	buffer = [0]*PACKET_SIZE
+	buffer[COMMAND_OFFSET] = CMD_END
+	packet = bytes(buffer)
+	device.write(ENDPOINT_OUT, packet)
+
+def _get_buffer_sum(buffer: list) -> list:
+	""" Returns the sum of `buffer` from `COMMAND_OFFSET`, split over 2 bytes """
+	b_sum = sum(buffer[COMMAND_OFFSET:])
+	b_sum1 = b_sum % 256
+	b_sum2 = b_sum // 256
+	return [b_sum1, b_sum2]
---
+++ main.py
@@ -0,0 +1,125 @@
+import sys
+from typing import Generator, Callable
+
+import gmmk
+
+
+def run_func(f: Callable[..., None], args: list) -> None:
+	"""
+	Handles aquiring and releasing the GMMK and runs the gmmk function `f` on
+	`args`
+
+	Args:
+		f: Any of the gmmk.set_* functions
+		args: arguments for `f`, excluding the device
+	"""
+	device = None
+	try:
+		device = gmmk.get_gmmk()
+		gmmk.setup(device)
+		f(device, *args)
+	finally:
+		if device:
+			# Give control back to whatever had it
+			device.attach_kernel_driver(0)
+
+def main() -> None:
+	if len(sys.argv) <= 1:
+		_show_help()
+		return
+	command = sys.argv[1]
+	args = sys.argv[2:]
+
+	if command in ["--help", "-h"]:
+		_show_help()
+	elif command in ["--mode", "-m"]:
+		if len(args) == 0:
+			print("Error: Command '--mode', '-m' requires 1 argument")
+			return
+		run_func(gmmk.set_mode, [int(args[0])])
+	elif command in ["--brightness", "-b"]:
+		if len(args) == 0:
+			print("Error: Command '--brightness', '-b' requires 1 argument")
+			return
+		run_func(gmmk.set_brightness, [int(args[0])])
+	elif command in ["--delay", "-d"]:
+		if len(args) == 0:
+			print("Error: Command '--delay', '-d' requires 1 argument")
+			return
+		run_func(gmmk.set_delay, [int(args[0])])
+	elif command in ["--left", "-l"]:
+		run_func(gmmk.set_direction_left, [])
+	elif command in ["--right", "-r"]:
+		run_func(gmmk.set_direction_right, [])
+	elif command in ["--colorful", "-f"]:
+		run_func(gmmk.set_colorful, [])
+	elif command in ["--single", "-s"]:
+		run_func(gmmk.set_not_colorful, [])
+	elif command in ["--rate", "-z"]:
+		if len(args) == 0:
+			print("Error: Command '--rate', '-z' requires 1 argument")
+			return
+		run_func(gmmk.set_rate, [int(args[0])])
+	elif command in ["--color", "-c"]:
+		if len(args) < 3:
+			print("Error: Command '--color', '-c' requires 3 arguments")
+			return
+		run_func(gmmk.set_color, map(int, args))
+	elif command in ["--keys", "-k"]:
+		if len(args) == 0:
+			print("Error: Command '--keys', '-k' requires 1 argument")
+			return
+		start, count, colors = _read_key_colors(args[0])
+		run_func(gmmk.set_keys, (start, count, colors))
+	else:
+		print("Command not found")
+		_show_help()
+
+def _show_help() -> None:
+	str = "Commands:\n"
+	str += "	--help, -h		Shows this help text\n"
+	str += "	--mode NUM, -m		Set the mode (1-20)\n"
+	str += "	--brightness NUM, -b	Set the brightness (0-4)\n"
+	str += "	--delay NUM, -d		Set the delay between animation frames. Smaller values are more meaningful (0-255)\n"
+	str += "	--left, -l		Set animation to proceed towards the left\n"
+	str += "	--right, -r		Set animation to proceed towards the right\n"
+	str += "	--colorful, -f		Turn on colorful mode\n"
+	str += "	--single, -s		Turn on single color (not colorful) mode\n"
+	str += "	--rate NUM, -z		Adjust polling rate (not tested): 0=125Hz, 1=250Hz, 2=500Hz, 3=1000Hz, 4+=?\n"
+	str += "	--color R G B, -c	Set color for single color mode (0-255)\n"
+	str += "	--keys FILE, -k		Set individual keys' colors according to FILE\n"
+	print(str)
+
+def _read_key_colors(filepath: str) -> list:
+	"""
+	Args:
+		filepath: path to the file specifying the key colors
+
+	Returns:
+		The number of the starting key, the number of keys and a list of
+		triples specifying the color each key should be
+	"""
+	with open(filepath, "r") as file:
+		values = _get_items(file.read())
+		start = int(next(values))
+		count = int(next(values))
+
+		colors = []
+		while True:
+			color = (
+				int(next(values, -1)),
+				int(next(values, -1)),
+				int(next(values, -1)) )
+			if -1 in color:
+				break
+			else:
+				colors.append(color)
+		return start, count, colors
+
+def _get_items(text: str) -> Generator[str, None, None]:
+	""" Yields the next string of non-whitespace characters """
+	for item in text.split():
+		yield item
+
+if __name__ == "__main__":
+	main()
