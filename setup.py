00e5a9d28ff6441021cf05c0d9e2cf17d306e9df
missing-semi-colon <67431858+missing-semi-colon@users.noreply.github.com>
2021-06-16 23:43:13 +0100

    Add install scripts
    Moved some files to make install scripts work. Also includes a PKGBUILD for Arch linux.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+	name="gmmkctl_py",
+	version="1.0",
+	url="https://github.com/missing-semi-colon/gmmkctl_py",
+	packages=["gmmkctl_py"],
+	install_requires=["pyusb", ],
+)
